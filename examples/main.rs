cfe2341bb52141e78ba3fcab77a693aac3029eab
kutakake <143584506+kutakake@users.noreply.github.com>
2025-07-01 18:10:01 +0900

    明示的に除去するタグの追加、それらのfor化、tag束縛位置の変更

--- examples/main.rs
+++ examples/main.rs
@@ -1,11 +1,24 @@
 use reader_mode_maker;
 use reqwest;
+use std::{thread, time};
 
 fn main() {
-
-    let url = "https://example.com";
+    let url = "https://gigazine.net/news/20250701-wordpress-matt-mullenweg-interview/";
     let response = reqwest::blocking::get(url).unwrap();
     let content = response.text().unwrap();
-    let reader_mode = reader_mode_maker::culling(&content);
-    println!("{}", reader_mode);
+    let now = time::Instant::now();
+    /*
+    for i in 0..1 {
+        let reader_mode = reader_mode_maker::old_culling(&content);
+    }
+    println!("{:?}", now.elapsed());
+    */
+    let now = time::Instant::now();
+    //for i in 0..1 {
+        let reader_mode = reader_mode_maker::culling(&content);
+    //}
+    println!("{:?}", &reader_mode);
+    println!("{:?}", now.elapsed());
+
+    //println!("{}", reader_mode);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,61 @@
 pub fn culling(input: &str) -> String {
     let html: Vec<char> = input.chars().collect();
     let mut output: String = String::new();
-    let mut tag = String::new();
     let html_length = html.len();
     let mut index = 0;
     let tags: Vec<&str> = vec![
-        "<title>", "</title>", "<br>", "<br />", "<h1", "</h1>", "<h2", "</h2>", "<h3", "</h3>", "<h4",
-        "</h4>", "<h5", "</h5>", "<h6", "</h6>", "<b>", "<b ", "</b>", "<i>", "<i ", "</i>", "<li>", "<li ",
-        "</li>", "<ul", "</ul", "<ol>", "<ol ", "</ol", "<code", "</code", "<pre>", "<pre ", "</pre>",
-        "<p>", "<p ", "</p>", "<a>", "<a ", "</a>"
+        "<title>", "</title>", "<br>", "<br />", "<h1", "</h1>", "<h2", "</h2>", "<h3", "</h3>",
+        "<h4", "</h4>", "<h5", "</h5>", "<h6", "</h6>", "<b>", "<b ", "</b>", "<i>", "<i ", "</i>",
+        "<li>", "<li ", "</li>", "<ul", "</ul", "<ol>", "<ol ", "</ol", "<code", "</code", "<pre>",
+        "<pre ", "</pre>", "<p>", "<p ", "</p>", "<a>", "<a ", "</a>",
     ];
     let number_of_tags = tags.len();
 
+    let ignore_list: Vec<&str> = vec![
+        "<script", "<style", "<iframe", "<noscript", "<symbol",
+    ];
+
+    let ignore_end_list: Vec<&str> = vec![
+        "</script>", "</style>", "</iframe>", "</noscript>", "</symbol>",
+    ];
+
+    let mut ignored = false;
+
+    let number_of_ignore_list = ignore_list.len();
+
     while index < html_length {
         if html[index] == '<' {
+            let mut tag = String::new();
             while html[index] != '>' {
                 tag.push(html[index]);
                 index += 1;
             }
             tag.push(html[index]);
             index += 1;
-            if tag.starts_with("<script") {
-                while !tag.ends_with("</script>") {
-                    tag.push(html[index]);
-                    index += 1;
-                }
-                tag = String::new();
-                continue;
-            }
 
-            if tag.starts_with("<style") {
-                while !tag.ends_with("</style>") {
-                    tag.push(html[index]);
-                    index += 1;
-                }
-                tag = String::new();
-                continue;
-            }
-            for i in 0..number_of_tags {
-                if tag.contains(tags[i]) {
-                    output = format!("{}{}", output, tag.chars().collect::<String>());
+
+            for i in 0..number_of_ignore_list {
+                if tag.to_lowercase().starts_with(ignore_list[i]) {
+                    while !tag.to_lowercase().ends_with(ignore_end_list[i]) && index < html_length {
+                        tag.push(html[index]);
+                        index += 1;
+                    }
+                    ignored = true;
                     break;
                 }
             }
 
-            tag = String::new();
+            if ignored {
+                ignored = false;
+                continue;
+            }
+
+            for i in 0..number_of_tags {
+                if tag.to_lowercase().contains(tags[i]) {
+                    output = format!("{}{}", output, tag.chars().collect::<String>());
+                    break;
+                }
+            }
         }
         output = format!("{}{}", output, html[index]);
         index += 1;
